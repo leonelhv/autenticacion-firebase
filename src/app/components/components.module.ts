1d723f732f85d25eaff91406d1c7ece5b27e0b4f
Leonel Huachaca <leonel.huachacav@gmail.com>
2023-01-24 17:12:00 -0500

    test overlay angular cdk

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { TablaComponent } from './tabla/tabla.component';
+import { OverlayComponent } from './overlay/overlay.component';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { PortalModule } from '@angular/cdk/portal';
+import { AddProductComponent } from './add-product/add-product.component';
 
 @NgModule({
-  declarations: [HeaderComponent, TablaComponent],
-  imports: [CommonModule],
-  exports: [HeaderComponent, TablaComponent],
+  declarations: [
+    HeaderComponent,
+    TablaComponent,
+    OverlayComponent,
+    AddProductComponent,
+  ],
+  imports: [CommonModule, OverlayModule, PortalModule],
+  exports: [
+    HeaderComponent,
+    TablaComponent,
+    OverlayComponent,
+    AddProductComponent,
+  ],
 })
 export class ComponentsModule {}
